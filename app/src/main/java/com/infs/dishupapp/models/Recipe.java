b87bcfd4878fadbe13da539b0a82199af5ce80c3
Chris Lau <christopher.w.lau@student.unsw.edu.au>
2019-11-18 21:37:54 +1100

    Hi this is to add RecipeDetailActivity class

--- app/src/main/java/com/infs/dishupapp/models/Recipe.java
+++ app/src/main/java/com/infs/dishupapp/models/Recipe.java
@@ -1,4 +1,80 @@
 package com.infs.dishupapp.models;
 
 public class Recipe {
+
+    public int idMeal;
+    public String strMeal;
+    public String strCategory;
+    public String strInstructions;
+    public String strMealThumb;
+    public String strIngredients1;
+    public String strIngredients2;
+    public String strIngredients3;
+    public String strIngredients4;
+    public String strIngredients5;
+    public String strMeasure1;
+    public String strMeasure2;
+    public String strMeasure3;
+    public String strMeasure4;
+    public String strMeasure5;
+
+    public int getIdMeal() {
+        return idMeal;
+    }
+
+    public String getStrMeal() {
+        return strMeal;
+    }
+
+    public String getStrCategory() {
+        return strCategory;
+    }
+
+    public String getStrInstructions() {
+        return strInstructions;
+    }
+
+    public String getStrMealThumb() {
+        return strMealThumb;
+    }
+
+    public String getStrIngredients1() {
+        return strIngredients1;
+    }
+
+    public String getStrIngredients2() {
+        return strIngredients2;
+    }
+
+    public String getStrIngredients3() {
+        return strIngredients3;
+    }
+
+    public String getStrIngredients4() {
+        return strIngredients4;
+    }
+
+    public String getStrIngredients5() {
+        return strIngredients5;
+    }
+
+    public String getStrMeasure1() {
+        return strMeasure1;
+    }
+
+    public String getStrMeasure2() {
+        return strMeasure2;
+    }
+
+    public String getStrMeasure3() {
+        return strMeasure3;
+    }
+
+    public String getStrMeasure4() {
+        return strMeasure4;
+    }
+
+    public String getStrMeasure5() {
+        return strMeasure5;
+    }
 }
