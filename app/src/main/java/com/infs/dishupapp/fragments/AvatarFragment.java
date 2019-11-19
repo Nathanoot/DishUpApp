d1d1f03ad47d8cb202265c79a34ede8fd81ed11b
drusali <55827075+drusali@users.noreply.github.com>
2019-11-19 21:40:21 +1100

    avatar class

--- app/src/main/java/com/infs/dishupapp/fragments/AvatarFragment.java
+++ app/src/main/java/com/infs/dishupapp/fragments/AvatarFragment.java
@@ -4,20 +4,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.infs.dishupapp.R;
+import com.infs.dishupapp.models.Avatar;
 
 public class AvatarFragment extends Fragment {
+    private TextView name;
+    private TextView score;
+    private ImageView character;
 
-    //Temporary to Test Navigation
-    @Nullable
+    public AvatarFragment(){
+    }
+    @NonNull
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_avatar, container, false);
+    public View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_avatar, container, false);
+        name= view.findViewById(R.id.userName);
+        character= view.findViewById( R.id.imageAvatar);
+        //setting up scoring system
+        final Avatar avatar= new Avatar();
+        if(avatar.getScore()<2){
+            character.setImageResource(@drawable/baby.png );
+        }
+
 
     }
-}
+    }
+
--- app/src/main/java/com/infs/dishupapp/models/Avatar.java
+++ app/src/main/java/com/infs/dishupapp/models/Avatar.java
@@ -1,4 +1,26 @@
 package com.infs.dishupapp.models;
 
+import android.widget.ImageView;
+
 public class Avatar {
+    private String name;
+    private int score;
+    private ImageView character;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
+
