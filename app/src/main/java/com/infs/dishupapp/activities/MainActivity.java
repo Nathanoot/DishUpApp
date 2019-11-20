eca65044199d7ff16de53652f3f55f1724df136a
Nathanoot <randomgamerandom@hotmail.com>
2019-11-20 21:38:21 +1100

    Added recipeDetailActivity to manifest

--- app/src/main/java/com/infs/dishupapp/activities/MainActivity.java
+++ app/src/main/java/com/infs/dishupapp/activities/MainActivity.java
@@ -14,7 +14,7 @@ import com.infs.dishupapp.R;
 import com.infs.dishupapp.fragments.AvatarFragment;
 import com.infs.dishupapp.fragments.CategoryFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  {
 
     private BottomNavigationView bottomNavigationView;
     @Override
