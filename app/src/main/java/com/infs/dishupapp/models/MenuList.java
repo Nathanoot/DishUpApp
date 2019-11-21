0f0171587652f6218f0077d1901c1cdb921da0ab
Chris Lau <christopher.w.lau@student.unsw.edu.au>
2019-11-21 14:09:23 +1100

    Hi this is to add RecipeDetailActivity class

---
+++ app/src/main/java/com/infs/dishupapp/models/MenuList.java
@@ -0,0 +1,12 @@
+package com.infs.dishupapp.models;
+
+import java.util.ArrayList;
+
+public class MenuList {
+
+    public ArrayList<CategoryItem> meals;
+
+    public ArrayList<CategoryItem> getResults() {
+        return meals;
+    }
+}
